4d90dffebdaafb3f09aaa45a9922fe22fa31fb48
kiennd57 <kienduynguyen94@gmail.com>
2014-08-30 15:56:05 +0700

    fix detail of subject page!

--- protected/views/FuckRails/FuckRails.php
+++ protected/views/FuckRails/FuckRails.php
@@ -8,33 +8,48 @@
 
                         <div class="g-cols">
                             <div class="two-thirds">
+                                <h3>Thông tin chi tiết</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.</p>
 
                                 <div class="w-video ratio_16-9">
                                     <div class="w-video-h">
                                         <iframe src="http://player.vimeo.com/video/48363485"></iframe>
                                     </div>
                                 </div>
-
-                                <h3>Thông tin chi tiết</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.</p>
+                                <p style="text-align: center">Video 1: ....</p>
+                                <div class="w-video ratio_16-9">
+                                    <div class="w-video-h">
+                                        <iframe src="http://player.vimeo.com/video/48363485"></iframe>
+                                    </div>
+                                </div>
+                                <p style="text-align: center">Video 2: ....</p>
+                                <div class="underline1"></div>
+                                <h3>Các chủ đề liên quan</h3>
+                                <a href=""><i class="icon-arrow-right"></i> Data types and variables</a>
+                                <br/>
+                                <a href=""><i class="icon-arrow-right"></i> Control Flow</a>
+                                <br/>
+                                <a href=""><i class="icon-arrow-right"></i> Functions</a>
 
                             </div>
                             <div class="one-third">
                                 <div class="box more-box1">
-                                    <h6 style="color: #262626"><strong>DOWN LOAD</strong></h6>
-                                    <div class="underline2"></div>
-                                    <br/>
-                                    <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/book.png"/>
-                                    <a><i class="icon-arrow-down"></i> Slide 1 : Những điều cơ bản về C++</a>
-                                </div>
-
-                                <div class="box">
-                                    <h6 style="color: #262626;text-align: center"><strong>Bài tập - LAB</strong></h6>
-                                    <div class="underline2"></div>
-                                    <br/>
-                                    <a href=""><i class="icon-book"></i> Lab 1 - C++</a>
-                                    <br/>
-                                    <a href=""><i class="icon-book"></i> Đáp án - C++</a>
+                                    <h6 style="color: #262626"><strong>TÀI LIỆU</strong></h6>
+                                    <div class="underline3"></div>
+                                    <div class="block">
+                                        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/book.png"/>
+                                        <a><i class="icon-arrow-down"></i> Slide 1 : Những điều cơ bản về C++</a>
+                                    </div>
+                                    <div class="underline3"></div>
+                                    <div class="block">
+                                        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/book.png"/>
+                                        <a><i class="icon-arrow-down"></i> Slide 1 : Những điều cơ bản về C++</a>
+                                    </div>
+                                    <div class="underline3"></div>
+                                    <div class="block">
+                                        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/book.png"/>
+                                        <a><i class="icon-arrow-down"></i> Slide 1 : Những điều cơ bản về C++</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
