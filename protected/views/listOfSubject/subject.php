25bb0b1a426d71c5eaa68d0d165467ecf8f5cafd
Huy Nguyen <huynt57@gmail.com>
2014-08-31 21:29:15 +0700

    fix subject

--- protected/views/listOfSubject/subject.php
+++ protected/views/listOfSubject/subject.php
@@ -5,7 +5,12 @@
             <div class="g-cols">
 
                 <div class="full-width">
-                    <div class="head"><?php echo $subject->subject_name ?></div>
+                    <div class="head"><?php echo $subject->subject_name ?> <?php
+                        if ($subject->subject_requirement == "")
+                            echo '';
+                        else
+                            echo '(Môn học tiên quyết: '.$subject->subject_requirement.')';
+                        ?></div>
                     <div class="fb-like" data-href="<?php echo Yii::app()->createAbsoluteUrl('listOfSubject/subject?subject_id=') . $subject->subject_id ?>" data-layout="standard" data-action="like" data-show-faces="false" data-share="true"></div>
                     <div class="w-testimonial">
                         <div class="w-testimonial-h">
@@ -22,22 +27,23 @@
                         <div class="two-thirds" >
                             <strong style="color: #262626">Nội dung môn học</strong>
                             <p style="margin-right: 25px;margin-top: 20px;margin-bottom: 20px">
-                                <?php echo $subject->subject_content ?>
+    <?php echo $subject->subject_content ?>
                             </p>
                             <strong style="color: #262626">Giáo viên giảng dạy</strong>
                             <div class="g-cols" style="margin-right: 30px;margin-top: 20px">
-                                <?php foreach ($teacher as $teacher): ?>
+    <?php foreach ($teacher as $teacher): ?>
                                     <div class="one-third">
                                         <div class="w-team-member">
                                             <div class="w-team-member-h">
                                                 <div class="w-team-member-image">
                                                     <a href="<?php echo Yii::app()->createUrl('share/teacher?id=') . $teacher->teacher_id ?>">
-                                                        <img src="<?php if ($teacher->teacher_avatar != "") {
-                                echo $teacher->teacher_avatar;
-                            } else {
-                                echo Yii::app()->createAbsoluteUrl("themes/classic/assets/img/Teacher_img/Teacher_default_avatar.png");
-                            }
-                            ?>" alt="<?php echo $teacher->teacher_name ?>" />
+                                                        <img src="<?php
+                                                        if ($teacher->teacher_avatar != "") {
+                                                            echo $teacher->teacher_avatar;
+                                                        } else {
+                                                            echo Yii::app()->createAbsoluteUrl("themes/classic/assets/img/Teacher_img/Teacher_default_avatar.png");
+                                                        }
+                                                        ?>" alt="<?php echo $teacher->teacher_name ?>" />
                                                     </a>
                                                 </div>
                                                 <div class="w-team-member-meta">
@@ -67,15 +73,15 @@
 
                                 <h6> Tài liệu bắt buộc </h6>
                                 <div class="white">
-    <?php foreach ($doc as $doc): ?>
+                                    <?php foreach ($doc as $doc): ?>
                                         - <a href="google.com"><?php echo $doc->doc_name . ", " . $doc->doc_author . ", " . $doc->doc_publisher ?></a>
                                         <div class="underline1"></div>
-                                    <?php endforeach; ?>
+    <?php endforeach; ?>
                                 </div>
 
                                 <h6> Tài liệu tham khảo </h6>
                                 <div class="white">
-    <?php foreach ($reference as $reference): ?>
+                                    <?php foreach ($reference as $reference): ?>
                                         - <a href="<?php echo $reference->doc_url ?>"><?php echo $reference->doc_name . ", " . $reference->doc_author . ", " . $reference->doc_publisher ?></a>
                                         <div class="underline1"></div>
     <?php endforeach; ?>
@@ -91,7 +97,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php foreach ($lesson as $lesson): ?>
+    <?php foreach ($lesson as $lesson): ?>
                                         <tr style="border-bottom: 1px solid #d0d6d9">
                                             <td><a href="<?php echo Yii::app()->createUrl('lesson?lesson_id= ') . $lesson->lesson_id ?>"><?php echo $lesson->lesson_name ?></a></td>
                                             <td><?php echo $lesson->lesson_weeks ?></td>                            
