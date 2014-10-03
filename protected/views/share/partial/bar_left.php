3913a6322400bd18a6cf5e6916f3c61f85a8aed0
Huy Nguyen <huynt57@gmail.com>
2014-10-03 23:35:39 +0700

    bug side bar teacher

--- protected/views/share/partial/bar_left.php
+++ protected/views/share/partial/bar_left.php
@@ -3,8 +3,7 @@
      border-right-width: thick;
      border-color: #d0d6d9;
      ">
-
-    <h3 id="second">Các ngành học</h3>            
+    <h3>Các ngành học</h3>
     <div class="widget">
         <nav class="w-nav">
             <div class="w-nav-h">
@@ -12,13 +11,11 @@
                     <div class="w-nav-list-h">
                         <?php foreach ($category_father as $category): ?>
                             <div class="w-nav-item level_1 active">
-
                                 <?php $dept = Dept::model()->findAllByAttributes(array('dept_faculty' => $category->faculty_id)); ?>
                                 <div class="w-nav-item-h">
-                                    <a href="#faculty-<?php echo $category->faculty_id ?>" class="w-nav-anchor level_1 faculty" faculty-id="<?php echo $category->faculty_id ?>"><?php echo $category->faculty_name ?> 
+                                    <a href="#faculty-<?php echo $category->faculty_id ?>" class="w-nav-anchor level_1 faculty" faculty-id="<?php echo $category->faculty_id ?>"><?php echo $category->faculty_name ?>
                                         <span class="w-nav-title " ></span>
                                     </a>
-
                                     <?php
                                     if ($dept) {
                                         foreach ($dept as $dept_detail):
@@ -26,24 +23,8 @@
                                             <div class="w-tabs layout_accordion type_toggle">
                                                 <div class="w-tabs-h">
                                                     <div class="w-tabs-section with_icon">
-
                                                         <div class="w-tabs-section-title">
-                                                            <a href="#dept-<?php echo $category->faculty_id ?>-<?php echo $dept_detail->dept_id ?>"class="w-tabs-section-title-text dept" style="margin-left: 32px" faculty-id="<?php echo $category->faculty_id ?>" dept-id="<?php echo $dept_detail->dept_id ?>">- <?php echo $dept_detail->dept_name ?></a>
-                                                        </div>
-                                                        <div class="w-tabs-section-content">
-                                                            <div class="w-tabs-section-content-h">
-                                                                <?php foreach ($subject_type as $subject_detail): ?>
-                                                                    <div class="w-nav-item level_2 active">
-                                                                        <div class="w-nav-item-h">
-                                                                            <a href="#subject-<?php echo $category->faculty_id ?>-<?php echo $dept_detail->dept_id ?>-<?php echo $subject_detail->id ?>" class="w-nav-anchor level_2 subject" faculty-id="<?php echo $category->faculty_id; ?>" dept-id="<?php echo $dept_detail->dept_id ?>" subject-type="<?php echo $subject_detail->id ?>">
-                                                                                <span class="w-nav-title " >
-                                                                                    <?php echo $subject_detail->subject_type_name ?>
-                                                                                </span>
-                                                                            </a>
-                                                                        </div>
-                                                                    </div>
-                                                                <?php endforeach; ?>
-                                                            </div>
+                                                            <a href="#dept-<?php echo $category->faculty_id ?>-<?php echo $dept_detail->dept_id ?>" class="w-tabs-section-title-text dept" style="margin-left: 32px" faculty-id="<?php echo $category->faculty_id ?>" dept-id="<?php echo $dept_detail->dept_id ?>">- <?php echo $dept_detail->dept_name ?></a>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -55,25 +36,20 @@
                                                 <?php foreach ($subject_type as $subject_detail): ?>
                                                     <div class="w-nav-item level_2 active">
                                                         <div class="w-nav-item-h">
-
-                                                            <a href="#subject-<?php echo $category->faculty_id ?>-<?php echo $dept_detail->dept_id ?>-<?php echo $subject_detail->id ?>" class="w-nav-anchor level_2 subject" faculty-id="<?php echo $category->faculty_id; ?>" dept-id="<?php echo $dept_detail->dept_id ?>" subject-type="<?php echo $subject_detail->id ?>">
+                                                            <a href="#dept-<?php echo $category->faculty_id ?>-<?php echo $dept_detail->dept_id ?>" class="w-nav-anchor level_2 subject" faculty-id="<?php echo $category->faculty_id; ?>" dept-id="<?php echo $dept_detail->dept_id ?>" subject-type="<?php echo $subject_detail->id ?>">
                                                                 <span class="w-nav-title" >
                                                                     <?php echo $subject_detail->subject_type_name ?>
                                                                 </span>
                                                             </a>
-
                                                         </div>
                                                     </div>
                                                 <?php endforeach; ?>
-
                                             </div>
                                         </div>
                                     <?php } ?>
-
                                 </div>
                             </div>
-
-                        <?php endforeach; ?>  
+                        <?php endforeach; ?>
                     </div>
                 </div>
             </div>
