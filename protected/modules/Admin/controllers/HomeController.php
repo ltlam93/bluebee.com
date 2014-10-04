55bfe5bfb8e6d15d2beab6f13c69c74f14c58ea8
Huy Nguyen <huynt57@gmail.com>
2014-10-04 18:13:09 +0700

    fix admin

--- protected/modules/Admin/controllers/HomeController.php
+++ protected/modules/Admin/controllers/HomeController.php
@@ -1,26 +1,36 @@
 <?php
 
-class HomeController extends AdminTableController
-{
+class HomeController extends AdminTableController {
+
     var $pages = array(
-        "user" => array (
+        "user" => array(
             "title" => "User manager"
         ),
     );
-	public function actionIndex()
-	{
-		$this->render('index');
-	}
-        
-        protected function getFileLocation()
-        {
-            return __FILE__;
-        }
-        
-        public function actionUser() {
-            $this->setCurrentPage("user");
-            $this->handleTable("user");
-        }
-        
-        
+
+//    public function beforeAction($action) {
+//        if (Yii::app()->user->isGuest && $action=="login") {
+//            $this->redirect(Yii::app()->createUrl('/admin/home/login'));
+//            return false;
+//        }
+//        return parent::beforeAction($action);
+//    }
+
+    public function actionIndex() {
+        $this->render('index');
+    }
+
+    public function actionLogin() {
+        $this->render('login/index');
+    }
+
+    protected function getFileLocation() {
+        return __FILE__;
+    }
+
+    public function actionUser() {
+        $this->setCurrentPage("user");
+        $this->handleTable("user");
+    }
+
 }
