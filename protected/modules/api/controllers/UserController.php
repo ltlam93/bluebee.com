e05a400e8cac96b022d3ac206c8c0db6810bdb60
Nguyễn Thế Huy <huynt57@gmail.com>
2015-03-20 22:14:39 +0700

    fix api save user

--- protected/modules/api/controllers/UserController.php
+++ protected/modules/api/controllers/UserController.php
@@ -17,23 +17,37 @@ class UserController extends Controller {
 
                 $check = User::model()->findByAttributes(array('user_id_fb' => $user_id_fb));
                 $user_dob = $request->getPost('user_dob');
-
+                $user_id = "";
                 if ($check) {
                     $check->user_avatar = "http://graph.facebook.com/" . $user_id_fb . "/picture?type=large";
+                    $check->user_id_fb  = $user_id_fb;
                     $this->retVal->user_data = $check;
                     $this->retVal->message = 'true';
+                    $check->save(FALSE);
+                    $user_id = $check->user_id;
+                    $this->retVal->user_data = $check;
                 } else {
                     $user = new User;
                     $user->user_avatar = "http://graph.facebook.com/" . $user_id_fb . "/picture?type=large";
                     $user->user_dob = $user_dob;
-
+                    $user->user_id_fb = $user_id_fb;
                     if ($user->save(FALSE)) {
                         $this->retVal->user_data = $user;
                         $this->retVal->message = 'true';
+                        $user_id = $user->user_id;
                     } else {
                         $this->retVal->message = 'false';
                     }
                 }
+
+                $criteria = new CDbCriteria();
+                $criteria->select = "*";
+                $criteria->condition = "doc_author = :doc_author";
+                $criteria->params = array(':doc_author' => $user_id);
+               
+                $count = Doc::model()->count($criteria);
+                
+                $this->retVal->doc_count = $count;
             } catch (exception $e) {
                 $this->retVal->message = $e->getMessage();
             }
